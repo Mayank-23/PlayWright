73156d01a97a0c730588605cb12c82c8d2b71399
Mayank-23 <mayank.nigam23@gmail.com>
2026-05-23 00:05:16 +0530

    using Signin and Locators

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -5,15 +5,25 @@ test.only('Browser Context PlayWright test', async function({browser})
 { 
     const context = await browser.newContext(); //fresh instance of browser will open
     const page = await context.newPage();
+    const userName = page.locator('input#username');
+    const password = page.locator("[type='password']");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    await page.locator('input#username').fill("Mayank");
-    await page.locator("[type='password']").fill("learning");
+    await userName.fill("Mayank");
+    await password.fill("learning");
     await page.locator('#signInBtn').click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(await page.locator("[style*='block']")).toContainText('Incorrect');
-
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await password.fill("");
+    await password.fill("Learning@830$3mK2");
+    await page.locator('#signInBtn').click();
+  //  console.log(await page.locator(".card-body .card-title a").textContent()); //Playwright will give failure as there are 4 elements present with the written css
+     console.log(await page.locator(".card-body .card-title a").first().textContent());
+     console.log(await page.locator(".card-body .card-title a").nth(1).textContent());
+}),
     
-})
+
 
 test('Page Context Playwright Test', async ({page})=>{
     await page.goto("https://google.com");
