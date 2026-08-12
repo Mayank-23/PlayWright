0902eb7df3251c446f9f3aa766de013c2e5488c4
Mayank-23 <mayank.nigam23@gmail.com>
2026-08-12 22:42:07 +0530

    Assignment 1

--- tests/bookingFlowEventCreation.spec.js
+++ tests/bookingFlowEventCreation.spec.js
@@ -14,6 +14,13 @@ async function login(page, email = "assignment@user.com", password = "Learning@8
     await page.locator("#login-btn").click();
 }
 
+async function futureDatevalue() {
+    const future = new Date();
+    future.setDate(future.getDate()+7);
+    return future.toISOString().slice(0, 16);
+    
+}
+
 //Step 1
 test('Login to application', async({browser})=>{
     const context = await browser.newContext();
@@ -26,10 +33,20 @@ test('Login to application', async({browser})=>{
     await page.getByRole('button',{name:'Admin'}).click();
     await page.locator("[ href = '/admin/events' ]").first().click();
     const event = `Test Event ${Date.now()}`;  //Javascript function to get the timestamp in miliseconds and appending it with Test event to create event name
-    console.log(event);
+    //console.log(event);
     await page.locator('#event-title-input').fill(event);
     await page.getByPlaceholder('Describe the event…').fill("Test text entered into the event description");
+    //Start after Description
+    await page.getByLabel('city').fill("Bangalore");
+    await page.getByLabel('Venue').fill("MG Road");
+    await page.getByLabel('Event Date & Time').fill(await futureDatevalue());
+    await page.getByLabel('Price ($)').fill("1000");
+    await page.getByLabel('Total Seats').fill("50");
+    await page.locator('#add-event-btn').click();
+    await expect(page.getByText('✓Event created!×')).toBeVisible();
     //await page.pause();
+
+
 
 
 })
