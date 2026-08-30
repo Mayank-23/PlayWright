0738aaf3969646334f2e4840db578e1c70bfa856
Mayank-23 <mayank.nigam23@gmail.com>
2026-08-30 16:55:10 +0530

    Frames

--- tests/moreValidations.spec.js
+++ tests/moreValidations.spec.js
@@ -1,19 +1,20 @@
 const {test, expect} = require('@playwright/test')
 
 
-test('Popup Validations', async({page})=>{
+test('dialog handling, hovering and frames learning', async({page})=>{
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-    //await page.goto("https://www.google.com/");
-    //await page.goBack();
-    //await page.goForward();
     await expect(page.locator('#displayed-text')).toBeVisible();
     await page.locator('#hide-textbox').click();
     await expect(page.locator('#displayed-text')).toBeHidden();
     await page.getByPlaceholder('Enter Your Name').fill('Jimmy');
-    await page.pause();
     page.on('dialog', dialog => dialog.accept()); //used to handle the dialog or popups on the screen dialog.reject can be given for negative response to popup
     await page.locator('#confirmbtn').click();
     await page.locator('#mousehover').hover();
+    const framesPage = await page.frameLocator('#courses-iframe');
+    await framesPage.locator("li a[href='lifetime-access']:visible").click();
+    console.log(await framesPage.locator('.text h2 span').textContent());
+    const subscriber = await framesPage.locator('.text h2 span');
+    await expect(subscriber).toBeVisible();
     
 })
