f79cc41967688b0fdb583d44fe3a14a7af72e72b
Mayank-23 <mayank.nigam23@gmail.com>
2026-09-16 00:29:50 +0530

    Refactoring the code

--- tests/WebAPIPart2.spec.js
+++ tests/WebAPIPart2.spec.js
@@ -1,25 +1,22 @@
 const {test, expect, request} = require('@playwright/test');
-const { APIUtils } = require('./utils/apiUtils');
+const { apiUtils } = require('./utils/apiUtils');
 require('./utils/apiUtils');
 const loginPayLoad = {userEmail: "assignment@user.com", userPassword: "Learning@830$3mK2"} // Login payload given as a global constant object here which can be used for any of the test
-let token;
 const orderPayload = {"orders":[{"country":"Cuba","productOrderedId":"6960eac0c941646b7a8b3e68"}]} // Create order payload which has the item which needs to be placed in order
-let orderID;
+let response;
 
 test.beforeAll( async()=>{
   
     const apiContext = await request.newContext();  //here apiContent will work as page which we are using for UI test like page.locator and other things
-    const api = new APIUtils(apiContext, loginPayLoad);
-    api.createOrder(orderPayload);  
+    const api = new apiUtils(apiContext, loginPayLoad);
+    response = await api.createOrder(orderPayload);  
 });
 
-test('E2E Journey of eCommerce', async ({page})=>{
-    const APIUtils = new apiUtils(apiContext, loginPayLoad);
-    const orderID = createOrder(orderPayload)
+test('Place the order using API', async ({page})=>{
+
     await page.addInitScript(value => {   
         window.localStorage.setItem('token',value)  //used for injecting token into browser local storage
-    }, token);
-    const productName = 'ZARA COAT 3';
+    }, response.token);
     await page.goto("https://rahulshettyacademy.com/client/");
    //console.log(orderID);
    await page.locator("ul [routerlink*='myorders']").click();
@@ -29,12 +26,12 @@ test('E2E Journey of eCommerce', async ({page})=>{
    const itemCount = await items.count();
    for(let i=0;i<itemCount;i++){
     let orderID_match = (await items.nth(i).textContent());
-    if(orderID_match===orderID){
+    if(orderID_match===response.orderID){
         await buttons.nth(i).click();
         break;
     }
    }
    const orderIDdetails = await page.locator(".col-text").textContent();
-   await expect(orderID.includes(orderIDdetails)).toBeTruthy();
+   expect(response.orderID.includes(orderIDdetails)).toBeTruthy();
 
 })
--- tests/utils/apiUtils.js
+++ tests/utils/apiUtils.js
@@ -1,4 +1,4 @@
-class APIUtils{
+class apiUtils{
 
     constructor(apiContext, loginPayLoad){
         this.apiContext = apiContext;
@@ -14,24 +14,27 @@ class APIUtils{
         
             })
         const loginResponseJson = await loginResponse.json(); //.json is a method which will fetch the response of the API after the call is made
-        token = loginResponseJson.token; // here with .token we are fetching the token from the response body
+        const token = loginResponseJson.token; // here with .token we are fetching the token from the response body
         //console.log(token);
         return token;
     }
 
     async createOrder(orderPayload){
+        let response = {};
+        response.token = await this.getToken();
         const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
                 
                 data:orderPayload,
                 headers: {
-                    'Authorization' : this.getToken(), // Token is given so that order is placed for the particular user because the token holds the details of the logged in user
+                    'Authorization' : response.token, // Token is given so that order is placed for the particular user because the token holds the details of the logged in user
                     'Content-Type' : 'application/json' //
                 },
             })
             const orderResponseJSON = await orderResponse.json();
-            orderID = orderResponseJSON.orders[0]; //Our order ID is at 0 index
-            return orderID;
+            const orderID = orderResponseJSON.orders[0]; //Our order ID is at 0 index
+            response.orderID = orderID;
+            return response;
     }
 
 }
-module.exports = {APIUtils}
+module.exports = {apiUtils}
